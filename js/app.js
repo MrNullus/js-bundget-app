cb7f4c673d344a642aa74c38d2a2676fc9ca6d40
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-01-07 21:53:05 -0300

    modify app.js

--- js/app.js
+++ js/app.js
@@ -15,4 +15,209 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  // submit budget method
+  submitBudgetForm() {
+    const value = this.budgetInput.value;
+
+    if (value === "" || value < 0) {
+      this.budgetFeedback.classList.add("showItem");
+      this.budgetFeedback.innerHTML = `
+        <p>value cannot be emtpy or negative</p>
+      `;
+      const self = this;
+
+      setTimeout(function () {
+        self.budgetFeedback.classList.remove("showItem");
+      }, 3000);
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = "";
+      this.showBalance();
+    }
+  }
+
+  // show balance
+  showBalance() {
+    // console.log(`hey i'm getting a hold of 'this' keyword`);
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+
+    if (total < 0) {
+      this.balance.classList.remove("showGreen", "showBlack");
+      this.balance.classList.add("showRed");
+    } else if (total > 0) {
+      this.balance.classList.remove("showRed", "showBlack");
+      this.balance.classList.add("showGreen");
+    } else if (total === 0) {
+      this.balance.classList.remove("showGreen", "showRed");
+      this.balance.classList.add("showBlack");
+    }
+  }
+
+  // submit expense
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    console.log(amountValue);
+
+    if (expenseValue === "" || amountValue === "" || amountValue < 0) {
+      this.expenseFeedback.classList.add("showItem");
+      this.expenseFeedback.innerHTML = `
+        <p>values cannot be empty or negative</p>
+      `;
+      const self = this;
+      setTimeout(function () {
+        self.expenseFeedback.classList.remove("showItem");
+      }, 3000);
+    } else {
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount,
+      };
+
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+
+  // add expanse
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+            <h6 class="expense-title mb-0 text-uppercase list-item">
+                ${expense.title}
+            </h6>
+            <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+            <div class="expense-icons list-item">
+                <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+                  <i class="fas fa-edit"></i>
+                </a>
+                <a href="#" class="delete-icon" data-id="${expense.id}">
+                  <i class="fas fa-trash"></i>
+                </a>
+            </div>
+        </div>
+    `;
+
+    this.expenseList.appendChild(div);
+  }
+
+  //calculate total expense
+  totalExpense() {
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce(function (acc, curr) {
+        acc += curr.amount;
+        return acc;
+      }, 0);
+    }
+
+    this.expenseAmount.textContent = total;
+    return total;
+  }
+
+  // edit expense
+  editExpense(element) {
+    let id = parseInt(element.dataset.id);
+    let parent = element.parentElement.parentElement.parentElement;
+
+    // remove from DOM
+    this.expenseList.removeChild(parent);
+    // remove from the list
+    let expense = this.itemList.filter(function (item) {
+      return item.id === id;
+    });
+
+    // show value
+    this.expenseInput.value = expense[0].title;
+    this.amountInput.value = expense[0].amount;
+    // remove from the list
+    let tempList = this.itemList.filter(function (expense) {
+      return expense.id !== id;
+    });
+
+    this.itemList = tempList;
+    this.showBalance();
+  }
+
+  // delete expense
+  deleteExpense(element) {
+    let id = parseInt(element.dataset.id);
+    let parent = element.parentElement.parentElement.parentElement;
+    // remove from DOM
+    this.expenseList.removeChild(parent);
+
+    // delete ietm
+    let tempList = this.itemList.filter(function (expense) {
+      return expense.id !== id;
+    });
+    this.itemList = tempList;
+    this.showBalance();
+  }
 }
+
+function eventListener() {
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+  // new instance of UI
+  const ui = new UI();
+
+  // budget form submit
+  budgetForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    ui.submitBudgetForm();
+  });
+  // expense form submit
+  expenseForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    ui.submitExpenseForm();
+  });
+  // expense click
+  expenseList.addEventListener("click", function () {
+    if (event.target.parentElement.classList.contains("edit-icon")) {
+      ui.editExpense(event.target.parentElement);
+    } else if (event.target.parentElement.classList.contains("delete-icon")) {
+      ui.deleteExpense(event.target.parentElement);
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  eventListener();
+});
+
+//´´´´´´´´´´´¶¶¶$´´´´´´oø¢1
+//´´´´´´´´´ø¶o´´´¶7´7¶¶711o¶1
+//´´´´´´´´¶ø´´´´´o$¶$ø´´1´´o¶
+//´´´´´´´¶ø¢ø$$$øø¢oo¢øø$¶¶$¶$
+//´´´´´´¶øø¢ø$ø¢oo¢oøø$ø¢oooo¶
+//´´´´´ø¢ø¶ø71o¶¶$¶¶$o1oø¶øooø¶
+//´´´´¶$o´´´´´´´11´´´´´´´´¶ø¢o$¶
+//´´´øo$¶¶7¶¶´´´´´´´´´´´1´´7ø¢¢¶¶1
+//ø¶¶$¶¶¶¶¶¶$¢ø¶¶¶¶¶¶¶¶¶o71$$øoø¶¶ø
+//¶$o¶¶¶¶øo¢øo7$¶øøø¶ø¢¢øøøø¢o$¶¶¶$
+//´¶$7¶ø7øø´øø¶¢177o¢¶¶´oø¢¢øøø¶ø´
+//´´øøoø¶1´o$¶o¢øø¢ø¶7´1ø¢¢øø$¶´
+//´´´$¶¶´´$¶¶ooo¢¢ø¶´´o¶øøø¢$¶
+//´´¶¶¶´´$$¶o¢¢¢¢ø¶´´¢¶¢øø¢ø¶´
+//´¶¶¶´´$ø¶oo¢¢¢ø¶7´o¶øø$¢ø¶1
+//¢¶¶1´$ø¶oo¢¢¢ø¶7´´ø¢¶oø¶o1
+//¶$¶´´¶ø$oooooø¶´´ø$o¶o¢¶¶
+//¶ø¢´´¶¢¢¢øøøø¶7´´¶¢ø$¢¶¶
+//¶ø¶´´$¢¢¢¢øø¶7´´ø$o¶$¶¶
+//¶¶ø´´ø¢¢¢¢¢ø7´´1¶¶¶¶ø´
+//´¶¶ø´¢¢¢¢¢øø¢´¢¶¶¢´
+//´´¶¶¶øø¢¢¢¢ø$¶¶¶
+//´´´´ø¶¶¶¶¶¶¶¶
